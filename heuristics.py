10c39df5e551bb042f8ac3123e26aba68065028b
Andrew Gu <amg11@williams.edu>
2025-11-27 12:16:36 -0500

    Tried my hand at completing part 9. Need to get a sub-9000-node admissible heuristic. I'm gonna leave that to y'all.

--- heuristics.py
+++ heuristics.py
@@ -11,4 +11,35 @@ def eight_puzzle_heuristic(state, search_space):
     # At depth 13, BFS went through 758195 nodes. A* went through 1010. 
 
 def food_heuristic(state, search_space):
+    """potential heuristics:
+    1. num remaining food pellets
+    2. distance to closest pellet
+    3. average distance to all remaining pellets
+    4. distance to farthest pellet (better than sum of distance to all remaining pellets, which can overestimate)
+    5. distance to closest pellet + Minimum Spanning Tree over remaininig pellets? --> would need to modify search space to actually store that MST
+    """
+    # 1. Num remaining food pellets
+    # num_food = len(state[1]) # frozenset of remaining food positions
+    # return num_food
+    # 12385 nodes visited w/tricky maze,  food heuristic = num remaining food pellets
+
+    pacman_pos = state[0]
+    food_positions = state[1]
+    if len(food_positions) == 0:
+        return 0
+    farthest_distance = 0
+    for food_pos in food_positions:
+        distance = abs(pacman_pos[0] - food_pos[0]) + abs(pacman_pos[1] - food_pos[1])  # Manhattan distance
+        if distance > farthest_distance:
+            farthest_distance = distance
+    return farthest_distance
+
+    # closest distance gets us 13869 nodes visited
+    # avg distnace gets us 11245 nodes visited
+    # farthest distance gets us 9509 nodes visited
+    # farthest distance + remaining food after that gets us 8389 nodes visited -- but not admissible
+    
+
+
+
     return 0  # replace with a better heuristic!
